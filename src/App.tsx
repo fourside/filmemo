5466ebab0cbb6a5422cf547ac35677eed1e32260
fourside <fourside@gmail.com>
2020-03-27 10:58:32 +0900

    remove files and unused lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Amplify from "aws-amplify";
+import awsmobile from "./aws-exports";
+
 import './App.css';
 
-function App() {
+Amplify.configure(awsmobile);
+
+export const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      app
     </div>
   );
-}
+};
 
-export default App;
