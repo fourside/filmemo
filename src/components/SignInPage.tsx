9d2820e736a2356dad8f7f0b6dcd009227d671af
fourside <fourside@gmail.com>
2020-04-07 20:09:46 +0900

    use material ui in sign in page

--- src/components/SignInPage.tsx
+++ src/components/SignInPage.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+
 import { signInGoogle } from "../amplify/Auth";
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      marginTop: "2em",
+    },
+    button: {
+    },
+  }),
+);
 const SignInPage: React.FC = () => {
+  const classes = useStyles();
   return (
-    <>
-      <h2>Sign in</h2>
-      <button onClick={signInGoogle}>Open Google</button>
-    </>
+    <Container maxWidth="md" className={classes.root}>
+      <Typography variant="h5" gutterBottom>Sign in</Typography>
+      <Button
+        onClick={signInGoogle}
+        variant="contained"
+        color="primary"
+        className={classes.button}
+      >
+        Open Google
+      </Button>
+    </Container>
   );
 };
 
