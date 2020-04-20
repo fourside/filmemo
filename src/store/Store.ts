3ef9aa4467101bab532f4649eff15b6a96049d9e
fourside <fourside@gmail.com>
2020-04-20 23:54:32 +0900

    redux logger for develop

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import { rootReducer } from "../reducers/reducer";
 
+const isDebug = process.env.NODE_ENV !== "production";
+const middleware = [];
+if (isDebug) {
+  middleware.push(logger);
+}
+middleware.push(thunk);
 export const store = createStore(
   rootReducer,
-  applyMiddleware(
-    thunk,
-  ),
+  applyMiddleware(...middleware),
 );
