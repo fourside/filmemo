6309d6da9a01084d0c87e3f481116ce303023f5e
fourside <fourside@gmail.com>
2020-04-21 11:03:40 +0900

    config store, not bundle redux logger in production

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,15 +1,8 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import logger from "redux-logger";
-import { rootReducer } from "../reducers/reducer";
 
-const isDebug = process.env.NODE_ENV !== "production";
-const middleware = [];
-if (isDebug) {
-  middleware.push(logger);
+let storeModule;
+if (process.env.NODE_ENV === "production") {
+  storeModule = require("./StoreProd");
+} else {
+  storeModule = require("./StoreDev");
 }
-middleware.push(thunk);
-export const store = createStore(
-  rootReducer,
-  applyMiddleware(...middleware),
-);
+export const store = storeModule.store;
